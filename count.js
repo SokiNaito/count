50cc6bf1bd96de23f06e5841afe5a41e7e18f1a5
SokiNaito <145862656+SokiNaito@users.noreply.github.com>
2023-11-27 18:20:31 +0900

    Update count.js

--- count.js
+++ count.js
@@ -11,9 +11,9 @@ window.addEventListener('DOMContentLoaded', function() {
         // 文字数によって分岐
         if (len <= max) {
             res = `文字数はOKです! 文字数は${len}です。`;
-        } else {
+        } else  {
             res = `文字数が長すぎます！ ${len2}文字オーバーです。`;
-        }
+        } 
 
         document.querySelector('#output').innerHTML = res;
     });
@@ -53,5 +53,50 @@ window.addEventListener('DOMContentLoaded', function() {
         document.querySelector('#output').innerHTML = res;
     });
 
-
+    
+    
 });
+function clearTextarea() {
+    var textareaForm = document.getElementById("input");
+  textareaForm.value = '';
+}
+
+
+
+function load() {
+    if(!localStorage.getItem('mydata')) {
+    alert("保存データはありません");
+    } else {
+    mydata = localStorage.getItem('mydata');
+    document.getElementById("mydata_in").value = mydata;
+    }
+    }
+    
+    // 保存
+function save() {
+    var mydata = document.getElementById("mydata_in").value;
+    localStorage.setItem('mydata', mydata);
+    }
+    // 確認
+    function view() {
+    if(!localStorage.getItem('mydata')) {
+    alert("保存データはありません");
+    } else {
+    mydata = localStorage.getItem('mydata');
+    document.getElementById("mydata_out").innerText = mydata;
+    }
+    }
+    
+
+
+
+    const copy = () => {
+        // テキストエリアの文字を取得
+        const txt = document.getElementById("input").value;
+        console.log(txt);
+      
+        // クリップボードにコピー
+        navigator.clipboard.writeText(txt);
+
+        alert("コピーしました！");
+      };
