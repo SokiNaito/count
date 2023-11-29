fcad16a31841f57176138095356428baae710e37
SokiNaito <145862656+SokiNaito@users.noreply.github.com>
2023-11-30 03:15:19 +0900

    Update count.js

--- count.js
+++ count.js
@@ -1,25 +1,51 @@
 window.addEventListener('DOMContentLoaded', function() {
+
+    document.querySelector('#nihyaku').addEventListener('click', function(){
+        const val = document.querySelector('#input').value;
+
+        let len0 = val.length; //スペースも1文字としてカウント
+        let len = val.replace(/[\n\s　]/g, "").length;   // スペースを数えない
+        let N = len0 - len ; //スペースの数
+        const max = 200;
+        let len2 = len - max;
+        let res = '';
+
+        // 文字数によって分岐
+        if (len0 <= max) {
+            res = `${len}/${max}文字でOKです！スペース込みだと${len0}/${max}文字です。`;
+            
+        } else if(len0 >= max && len <= max){
+            res = `スペースを含めて${len0}文字です。スペースが${N}文字含まれています。`;
+        }
+        else  {
+            res = `${len}/${max}文字です！ ${len2}文字オーバーです。スペースを含めると${len0}/${max}文字です。`;
+           
+        } 
+        document.querySelector('#output').innerHTML = res;
+    });
+
+
     document.querySelector('#sanbyaku').addEventListener('click', function() {
         const val = document.querySelector('#input').value;
         
 
-        // 変数の初期化
-        let len0 = val.length;
-        let len = val.replace(/[\n\s　]/g, "").length; //スペースを数えない
-
-        
-           // 文字列の長さを得る
-        const max = 300;
+        let len0 = val.length; //スペースも1文字としてカウント
+        let len = val.replace(/[\n\s　]/g, "").length;   // スペースを数えない
+        let N = len0 - len ; //スペースの数
+        const max = 200;
         let len2 = len - max;
         let res = '';
 
 
         // 文字数によって分岐
-        if (len <= max) {
-            res = ` 文字数は${len}文字でOKです！スペースを含めると${len0}文字です。`;
-           
-        } else  {
-            res = `${len}文字です！ ${len2}文字オーバーです。スペースを含めると${len0}文字です。`;
+        if (len0 <= max) {
+            res = `${len}/${max}文字でOKです！スペース込みだと${len0}/${max}文字です。`;
+            
+        } else if(len0 >= max && len <= max){
+            res = `スペースを含めて${len0}文字です。スペースが${N}文字含まれています。`;
+        }
+        else  {
+            res = `${len}/${max}文字です！ ${len2}文字オーバーです。スペースを含めると${len0}/${max}文字です。`;
            
         } 
         
@@ -30,43 +56,28 @@ window.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#yonhyaku').addEventListener('click', function(){
         const val = document.querySelector('#input').value;
 
-        let len0 = val.length;
-        let len = val.replace(/[\n\s　]/g, "").length;   // 文字列の長さを得る
-        const max = 400;
-        let len2 = len - max;
-        let res = '';
-
-        // 文字数によって分岐
-        if (len <= max) {
-            res = `文字数は${len}文字でOKです！スペースを含めると${len0}文字です。`;
-           
-        } else  {
-            res = `${len}文字です！ ${len2}文字オーバーです。スペースを含めると${len0}文字です。`;
-           
-        } 
-        document.querySelector('#output').innerHTML = res;
-    });
-
-
-    document.querySelector('#nihyaku').addEventListener('click', function(){
-        const val = document.querySelector('#input').value;
-
-        let len0 = val.length;
-        let len = val.replace(/[\n\s　]/g, "").length;   // 文字列の長さを得る
+        let len0 = val.length; //スペースも1文字としてカウント
+        let len = val.replace(/[\n\s　]/g, "").length;   // スペースを数えない
+        let N = len0 - len ; //スペースの数
         const max = 200;
         let len2 = len - max;
         let res = '';
 
         // 文字数によって分岐
-        if (len <= max) {
-            res = `文字数は${len}文字でOKです！スペースを含めると${len0}文字です。`;
-           
-        } else  {
-            res = `${len}文字です！ ${len2}文字オーバーです。スペースを含めると${len0}文字です。`;
+        if (len0 <= max) {
+            res = `${len}/${max}文字でOKです！スペース込みだと${len0}/${max}文字です。`;
+            
+        } else if(len0 >= max && len <= max){
+            res = `スペースを含めて${len0}文字です。スペースが${N}文字含まれています。`;
+        }
+        else  {
+            res = `${len}/${max}文字です！ ${len2}文字オーバーです。スペースを含めると${len0}/${max}文字です。`;
            
         } 
         document.querySelector('#output').innerHTML = res;
     });
+
+
 
     
     
@@ -76,32 +87,46 @@ function clearTextarea() {
   textareaForm.value = '';
 }
 
-
+//----------------------------文章保存-------------------------------//
 
 function load() {
-    if(!localStorage.getItem('mydata')) {
+   /* if(!localStorage.getItem('mydata')) {
     alert("保存データはありません");
-    } else {
+    } else {*/
     mydata = localStorage.getItem('mydata');
     document.getElementById("mydata_in").value = mydata;
+
+    mydataSecond = localStorage.getItem('mydataSecond');
+    document.getElementById("mydataSecond_in").value = mydataSecond;
+
+    mydataThird = localStorage.getItem('mydataThird');
+    document.getElementById("mydataThird_in").value = mydataThird;
     }
-    }
+    
     
     // 保存
 function save() {
     var mydata = document.getElementById("mydata_in").value;
     localStorage.setItem('mydata', mydata);
     alert("保存しました！");
+    
+    var mydataSecond = document.getElementById("mydataSecond_in").value;
+    localStorage.setItem('mydataSecond', mydataSecond);
+
+
+    var mydataThird = document.getElementById("mydataThird_in").value;
+    localStorage.setItem('mydataThird', mydataThird);
+   
     }
-    // 確認
-    function view() {
-    if(!localStorage.getItem('mydata')) {
-    alert("保存データはありません");
-    } else {
-    mydata = localStorage.getItem('mydata');
-    document.getElementById("mydata_out").innerText = mydata;
-    }
-    }
+
+
+
+    
+
+
+    
+        
+
     
 
 
